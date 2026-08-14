3e15b91d2fa888980073cf9d1af7e04efb7f2d06
mintychochip <mintychochip@users.noreply.github.com>
2026-08-14 01:47:15 -0700

    test: cover mysql database settings

--- common/src/test/java/com/azoth/territory/persist/DatabaseDialectTest.java
+++ common/src/test/java/com/azoth/territory/persist/DatabaseDialectTest.java
@@ -2,7 +2,7 @@ package com.azoth.territory.persist;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class DatabaseDialectTest {
     @Test
